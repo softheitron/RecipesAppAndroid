2f8b1d6332055590b54d08b46abe1d6dc104398a
heiterss <softheitron@gmail.com>
2024-12-25 18:26:39 +0500

    RA-06-data-base-entities-created-assets-added

---
+++ app/src/main/java/com/example/recipesapp/Category.kt
@@ -0,0 +1,8 @@
+package com.example.recipesapp
+
+data class Category(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val imageUrl: String,
+)
---
+++ app/src/main/java/com/example/recipesapp/Ingredient.kt
@@ -0,0 +1,7 @@
+package com.example.recipesapp
+
+data class Ingredient(
+    val quantity: String,
+    val unitOfMeasure: String,
+    val description: String,
+)
