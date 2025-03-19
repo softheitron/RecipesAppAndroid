9cf6eeb1443b1f3d9277c19adda38580dd670673
heiterss <softheitron@gmail.com>
2025-03-19 09:24:50 +0500

    RA-33-thread-pool-serializations-and-logs-added

--- app/src/main/java/com/example/recipesapp/model/Recipe.kt
+++ app/src/main/java/com/example/recipesapp/model/Recipe.kt
@@ -2,8 +2,11 @@ package com.example.recipesapp.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
 
 @Parcelize
+@Serializable
 data class Recipe(
     val id: Int,
     val title: String,
