f68a8de01031e933c08f87e95e90eebd7a0bf144
heiterss <softheitron@gmail.com>
2025-03-22 18:02:28 +0500

    RA-35-nothing-important

--- app/src/main/java/com/example/recipesapp/data/RecipeApiService.kt
+++ app/src/main/java/com/example/recipesapp/data/RecipeApiService.kt
@@ -9,17 +9,17 @@ import retrofit2.http.Query
 
 interface RecipeApiService {
     @GET("category")
-    fun getCategories() : Call<List<Category>>
+    fun getCategories(): Call<List<Category>>
 
     @GET("category/{id}")
-    fun getCategoriesById(@Path("id") id: Int) : Call<List<Category>>
+    fun getCategoriesById(@Path("id") id: Int): Call<List<Category>>
 
     @GET("category/{id}/recipes")
-    fun getRecipesByCategoryId(@Path("id") id: Int) : Call<List<Recipe>>
+    fun getRecipesByCategoryId(@Path("id") id: Int): Call<List<Recipe>>
 
     @GET("recipes")
-    fun getRecipesByIds(@Query("ids") ids: String) : Call<List<Recipe>>
+    fun getRecipesByIds(@Query("ids") ids: String): Call<List<Recipe>>
 
     @GET("recipe/{id}")
-    fun getRecipeById(@Path("id") id: Int) : Call<Recipe>
+    fun getRecipeById(@Path("id") id: Int): Call<Recipe>
 }
--- app/src/main/java/com/example/recipesapp/data/repository/RecipesRepository.kt
+++ app/src/main/java/com/example/recipesapp/data/repository/RecipesRepository.kt
@@ -84,6 +84,4 @@ class RecipesRepository {
             null
         }
     }
-
-
 }
